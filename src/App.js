0426c54fe1c7e62d3de0f9426848dffdd24c893d
Jeff Free <jefffreelance3@gmail.com>
2021-12-07 04:39:10 -0300

    :art: components added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import react from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
 //import "./App.css";
+
+const todos = [
+    { text: "Cortar cebolla", completed: false },
+    { text: "Tomar curso", completed: false },
+    { text: "Hacer cosas de eso", completed: false },
+];
 
 function App() {
     return (
         <>
             <TodoCounter />
             <TodoSearch />
+
             <TodoList>
-                <TodoItem />
+                {todos.map((todo) => (
+                    <TodoItem key={todo.text} text={todo.text} />
+                ))}
             </TodoList>
             <CreateTodoButton />
         </>
